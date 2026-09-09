0543c0bdc665e1ed384ff98f65a755240cc6ff87
Dinuka Lakshan <74536249+DrkCyph7@users.noreply.github.com>
2026-09-09 22:20:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  allowedDevOrigins: ['192.168.1.139'],
+  allowedDevOrigins: ['192.168.1.139', '192.168.1.142'],
 }
 
 export default nextConfig
